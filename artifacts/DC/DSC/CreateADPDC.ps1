f271fe06a74c1ec64fba70ca1df895425ab56bd2
Vincenzo Sciarra MS <reikor0x@users.noreply.github.com>
2022-09-09 14:50:08 +0200

    update

--- artifacts/DC/DSC/CreateADPDC.ps1
+++ artifacts/DC/DSC/CreateADPDC.ps1
@@ -62,20 +62,6 @@ configuration CreateADPDC
             AddressFamily  = 'IPv4'
             DependsOn      = "[WindowsFeature]DNS"
         }
-
-        xWaitforDisk Disk2
-        {
-            DiskNumber = 2
-            RetryIntervalSec =$RetryIntervalSec
-            RetryCount = $RetryCount
-        }
-
-        xDisk ADDataDisk {
-            DiskNumber  = 2
-            DriveLetter = "F"
-            DependsOn   = "[xWaitForDisk]Disk2"
-        }
-
         WindowsFeature ADDSInstall { 
             Ensure    = "Present" 
             Name      = "AD-Domain-Services"
@@ -99,10 +85,10 @@ configuration CreateADPDC
             DomainName                    = $DomainName
             DomainAdministratorCredential = $DomainCreds
             SafemodeAdministratorPassword = $DomainCreds
-            DatabasePath                  = "F:\NTDS"
-            LogPath                       = "F:\NTDS"
-            SysvolPath                    = "F:\SYSVOL"
-            DependsOn                     = @("[xDisk]ADDataDisk", "[WindowsFeature]ADDSInstall")
+            DatabasePath                  = "C:\NTDS"
+            LogPath                       = "C:\NTDS"
+            SysvolPath                    = "C:\SYSVOL"
+            DependsOn                     = @("[WindowsFeature]ADDSInstall")
         } 
 
     }
